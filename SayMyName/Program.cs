682d02bab0acc7a77c2b8ed03443d557b020c9d1
Ashton-Tolman <Tolman.ash22@gmail.com>
2026-01-20 07:41:29 -0700

    Add project files.

---
+++ SayMyName/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Ashton Tolman
+ * RCET 2265
+ * Spring 2026
+ * 
+namespace SayMyName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
