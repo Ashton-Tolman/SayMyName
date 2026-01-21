8c044294d7a9b1dc0eb0f1c2a338ff0d102bd94e
Ashton-Tolman <Tolman.ash22@gmail.com>
2026-01-21 08:37:51 -0700

    Add user input option with question

--- SayMyName/Program.cs
+++ SayMyName/Program.cs
@@ -2,13 +2,19 @@
  * RCET 2265
  * Spring 2026
  * https://github.com/Ashton-Tolman/SayMyName.git*/
+using System.Reflection.Metadata;
+
 namespace SayMyName
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userinput;
+            Console.WriteLine("What is your name?");
+            userinput =Console.ReadLine();
+            Console.WriteLine($"Your name is \"{userinput}\"");
+            Console.Read();
         }
     }
 }
